aa0c52dfc203e7dc9de871f10b6787690de503b2
Ed Charbeneau <ed.charbeneau@gmail.com>
2020-04-11 10:13:15 -0400

    Added MediaQuery component

---
+++ BlazorSize/MediaQuery.razor.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Threading.Tasks;
+
+namespace BlazorPro.BlazorSize
+{
+    public partial class MediaQuery : IDisposable
+    {
+        const string ns = "blazorSizeMedia";
+        [Inject] public IJSRuntime Js { get; set; }
+        [Parameter] public string Media { get; set; }
+        [Parameter] public bool Matches { get; set; }
+        [Parameter] public EventCallback<bool> MatchesChanged { get; set; }
+        [Parameter] public RenderFragment Matched { get; set; }
+        [Parameter] public RenderFragment Unmatched { get; set; }
+
+        private MediaQueryArgs internalMedia = new MediaQueryArgs();
+        private DotNetObjectReference<MediaQuery> DotNetInstance;
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                DotNetInstance = DotNetObjectReference.Create(this);
+                var mq = await Js.InvokeAsync<MediaQueryArgs>($"{ns}.addMediaQueryListener", Media, DotNetInstance);
+                internalMedia = mq;
+            }
+            await base.OnAfterRenderAsync(firstRender);
+        }
+
+        [JSInvokable(nameof(MediaQuery.MediaQueryChanged))]
+        public void MediaQueryChanged(MediaQueryArgs args)
+        {
+            if (args.Media == internalMedia.Media)
+            {
+                MatchesChanged.InvokeAsync(args.Matches);
+                internalMedia = args;
+                if (Matched != null || Unmatched != null)
+                {
+                    StateHasChanged();
+                }
+            }
+        }
+
+        public async void Dispose()
+        {
+            if (DotNetInstance != null)
+            {
+                await Js.InvokeVoidAsync($"{ns}.removeMediaQueryListeners", DotNetInstance);
+                DotNetInstance.Dispose();
+                DotNetInstance = null;
+            }
+        }
+    }
+}
