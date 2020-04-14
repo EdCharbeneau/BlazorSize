46b4a83b4e1edb13ecff1ccd264be60d2428cedb
Ed Charbeneau <ed.charbeneau@gmail.com>
2020-04-14 13:42:31 -0400

    migrated to latest Blazor client

--- BlazorSize.CsbExample/Program.cs
+++ BlazorSize.CsbExample/Program.cs
@@ -1,16 +1,26 @@
-﻿using Microsoft.AspNetCore.Blazor.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Blazor.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using BlazorPro.BlazorSize;
 
 namespace BlazorSize.CsbExample
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+            builder.Services.AddResizeListener(options =>
+            {
+                options.ReportRate = 300;
+                options.EnableLogging = false;
+                options.SuppressInitEvent = false;
+            });
+            await builder.Build().RunAsync();
         }
-
-        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
-            BlazorWebAssemblyHost.CreateDefaultBuilder()
-                .UseBlazorStartup<Startup>();
     }
 }
