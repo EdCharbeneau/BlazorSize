400c31e8312b2fe484a0b39b86e435b84f5153c7
Ed Charbeneau <ed.charbeneau@gmail.com>
2020-04-20 15:40:15 -0400

    Removed compiled files, these will get produced when "test, build, and bundle" run

--- BlazorSize/scripts/tests/blazorSize.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const blazorSizeMedia_js_1 = require("./blazorSizeMedia.js");
-const blazorSizeResize_js_1 = require("./blazorSizeResize.js");
-window.blazorSizeMedia = new blazorSizeMedia_js_1.BlazorSizeMedia();
-window.blazorSize = new blazorSizeResize_js_1.ResizeListener();
--- BlazorSize/wwwroot/blazorSize.js
+++
@@ -1,4 +0,0 @@
-import { BlazorSizeMedia } from './blazorSizeMedia.js';
-import { ResizeListener } from './blazorSizeResize.js';
-window.blazorSizeMedia = new BlazorSizeMedia();
-window.blazorSize = new ResizeListener();
