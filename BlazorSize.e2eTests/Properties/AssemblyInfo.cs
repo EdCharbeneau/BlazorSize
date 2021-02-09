9ad01fc6ea1be0552c961acc868fcb3a71fc7abd
Ed Charbeneau <ed.charbeneau@gmail.com>
2021-02-09 15:25:01 -0500

    Added e2e with Test Studio

---
+++ BlazorSize.e2eTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BlazorSize.e2eTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BlazorSize.e2eTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0241a973-6232-4fd2-a3f2-11cf65c27fea")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestComponents/wwwroot/fixture.js
@@ -0,0 +1,9 @@
+﻿var myWindow;
+
+function openWin(page, w, h) {
+    myWindow = window.open(`/${page}`, "", `width=${w}, height=${h}`);
+}
+
+function resizeWin(w, h) {
+    myWindow.resizeTo(w, h);
+}
