68343016c2c298e62b2151d342888c57e6096f82
Ed Charbeneau <ed.charbeneau@gmail.com>
2021-05-24 13:30:07 -0400

    BlazorSize.js is obsolete

--- BlazorSize/scripts/blazorSize.ts
+++
@@ -1,4 +0,0 @@
-﻿import { BlazorSizeMedia } from './blazorSizeMedia.js';
-import { ResizeListener } from './blazorSizeResize.js';
-(<any>window).blazorSizeMedia = new BlazorSizeMedia();
-(<any>window).blazorSize = new ResizeListener();
