17b6a1c91563a9df528b79b4ebf84cd8a8ed8ea7
Ed Charbeneau <ed.charbeneau@gmail.com>
2021-11-10 11:51:23 -0500

    Moved bUnit to external project. Updated to .NET 6.0

--- BlazorSize.UnitTesting/Bunit/FakeMediaQueryService.cs
+++ BlazorSize.UnitTesting/Bunit/FakeMediaQueryService.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
-using BlazorPro.BlazorSize;
-using Bunit;
+﻿using BlazorPro.BlazorSize;
 using Microsoft.JSInterop;
 
 namespace Bunit
