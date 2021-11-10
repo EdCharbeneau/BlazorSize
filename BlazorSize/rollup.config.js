93fed85cc38b0e3b792d1523d94e6e88ca8dffe4
Ed Charbeneau <ed.charbeneau@gmail.com>
2021-11-10 11:59:18 -0500

    Removed bundle/rollup. No longer needed with .NET 6.0. Removes .NET 5 support

--- BlazorSize/rollup.config.js
+++
@@ -1,8 +0,0 @@
-﻿import { terser } from "rollup-plugin-terser";
-
-export default {
-    input: "./wwwroot/blazorSize.js",
-    output: [
-        { file: "./wwwroot/blazorSize.min.js", format: "iife", plugins: [terser()] },
-    ]
-};
