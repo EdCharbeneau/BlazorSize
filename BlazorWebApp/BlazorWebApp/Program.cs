9917331f3458f0e73c0ae4768849e8cb9089325d
ed.charbeneau@gmail.com <ed.charbeneau@gmail.com>
2024-09-20 15:37:11 -0400

    Update to .NET 9.0, restructure solution, add Playwright
    Updated target framework to .NET 9.0 in multiple projects:
    - BlazorPro.BlazorSize.BlazorRepl.csproj
    - BlazorPro.BlazorSize.UnitTesting.csproj
    - BlazorPro.BlazorSize.csproj
    
    Updated package versions:
    - bunit.core to 1.31.3 in BlazorPro.BlazorSize.UnitTesting.csproj
    - BlazorPro.BlazorSize to 9.0.0-preview in BlazorPro.BlazorSize.csproj
    - .NET 9.0 RC packages in BlazorPro.BlazorSize.csproj
    
    Modified BlazorSize.sln:
    - Removed BlazorSize.e2eTests project
    - Added "REPL Support" and "Unit Testing" folders
    - Added PlaywrightTests project
    - Updated project nesting and build configurations
    
    Added Playwright for testing:
    - New package-lock.json and package.json
    - New PlaywrightTests.csproj
    - New UnitTest1.cs with Playwright tests
    
    Simplified NavMenu.razor, updated Program.cs, and modified Index.razor.

--- BlazorWebApp/BlazorWebApp/Program.cs
+++ BlazorWebApp/BlazorWebApp/Program.cs
@@ -6,6 +6,7 @@ using TestComponents;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
